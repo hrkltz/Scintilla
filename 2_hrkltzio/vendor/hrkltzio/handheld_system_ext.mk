91043d6c97584c81817a7b5e81e836d0e425720c
Oliver Herklotz <oliver@hrkltz.io>
2023-02-09 14:47:27 +0100

    Update

--- 2_hrkltzio/vendor/hrkltzio/handheld_system_ext.mk
+++ 2_hrkltzio/vendor/hrkltzio/handheld_system_ext.mk
@@ -1,5 +1,14 @@
 $(warning HRKLTZIO vendor/hrkltzio/handheld_system_ext.mk)
 
+# Another version for the check using $filter:
+#ifneq ($(filter redfin, $(TARGET_DEVICE)),)
+#    $(error Expected device of type redfin but got $(TARGET_DEVICE))
+#endif
+
+ifneq ($(TARGET_PRODUCT), sdk_phone_x86_64)
+    $(error Expected TARGET_PRODUCT to be sdk_phone_x86_64 but was $(TARGET_PRODUCT))
+endif
+
 PRODUCT_SOONG_NAMESPACES += \
     vendor/hrkltzio/prebuilts \
 
